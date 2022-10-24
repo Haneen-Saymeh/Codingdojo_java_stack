4919eb3de77b4147fce729f79c4f94012321ae72
Haninz <enghaneen_s@hotmail.com>
2022-10-24 14:46:38 +0300

    Alfred bot

---
+++ java_fundamentals/alfred_bot/AlfredQuotes.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        
+        return "Hello" + " " + name + " " + "nice to see you!";
+    }
+    
+    public String dateAnnouncement() {
+        Date datenow = new Date();
+        return "It's currently:" + datenow;
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+        if (conversation.contains("Alexis")){
+            return "Right away, sir.";
+
+        }
+
+        else if (conversation.contains("Alfred")){
+            return "At your service.";
+
+        }
+
+        else {
+            return "Right. And with that I shall retire." ;
+        }
+       
+    
+       
+    }
+    
+	
+}
+
